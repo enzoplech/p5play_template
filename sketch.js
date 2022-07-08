a7ef123334f686225e2c3c7892ae9f4e7b2b7c6b
enzoplech <enzoplech@gmail.com>
2022-07-08 17:59:52 -0300

    first commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,26 @@
-
+var caixa
 function setup() {
   createCanvas(400,400);
+  caixa =createSprite(200,200,50,50)
 }
 
 function draw() 
+
 {
   background(30);
+if(keyDown(LEFT_ARROW)){
+caixa.x = caixa.x -2
+}
+else if(keyDown(RIGHT_ARROW)){
+  caixa.x = caixa.x +2
+}
+else if(keyDown(UP_ARROW)){
+   caixa.y = caixa.y -2
+}
+else if(keyDown(DOWN_ARROW)){
+   caixa.y = caixa.y +2
+}
+  drawSprites()
 
 }
 
